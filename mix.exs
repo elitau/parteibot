768c982d78447959dbc168b90843276a0468137b
Eduard Litau <eduard.litau@gmail.com>
2017-02-05 22:55:39 +0100

    Get rid of hound warning upon boot

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Parteibot.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:hound, "~> 1.0", only: [:test, :dev]},
+     {:hound, "~> 1.0"},
      {:rollbax, "~> 0.8"},
      {:extwitter, "~> 0.8"},
      {:cowboy, "~> 1.0"}]
