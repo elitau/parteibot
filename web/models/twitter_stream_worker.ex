4decc9aecffdc669e97d6ae52c0fcf2c489b5af2
Eduard Litau <eduard.litau@gmail.com>
2017-02-21 14:27:51 +0100

    Learning about processes in background

--- web/models/twitter_stream_worker.ex
+++ web/models/twitter_stream_worker.ex
@@ -20,14 +20,29 @@ defmodule Parteibot.TwitterStreamWorker do
   end
 
   defp start_stream do
-    stream = ExTwitter.stream_filter(track: "Steuern")
-    IO.puts "Spawning stream watch process"
+    # hashtags = Parteibot.Repo.all(Parteibot.Hashtag)
+    # IO.puts "Spawning stream watch process for #{Enum.map(hashtags, &(&1.name))}"
     #   |> Stream.map(fn(x) -> x.text end)
     #   |> Stream.map(fn(x) -> IO.puts "#{x}\n---------------\n" end)
     # Enum.to_list(stream)
+    [p1 | [p2 | [ p3 | _tail] ] ] = Enum.map(["#apple", "#google", "#sony"], fn htag -> start_stream_for(htag)
+    end)
+    # head
+    # start_stream_for(p2)
+    # p2
+    IO.inspect p1
+    IO.inspect p2
+    IO.inspect p3
+    IO.puts "_tail #{_tail}"
+    p2
+  end
+
+  def start_stream_for(hashtag) do
+    IO.puts "Spawning stream watch process for #{hashtag}"
+    stream = ExTwitter.stream_filter(track: hashtag)
     spawn(
       fn -> stream
-            |> Stream.map(fn(tweet) -> send_reply(tweet) end)
+            |> Stream.map(fn(tweet) -> send_reply(tweet, hashtag) end)
             |> Enum.to_list
       end
     )
@@ -35,13 +50,13 @@ defmodule Parteibot.TwitterStreamWorker do
 
   # t :: %ExTwitter.Model.Tweet{contributors: term, in_reply_to_status_id_str: term, filter_level: term, favorite_count: term, withheld_scope: term, scopes: term, geo: term, display_text_range: term, retweeted: term, entities: term, id_str: term, quoted_status_id_str: term, quoted_status_id: term, in_reply_to_user_id_str: term, quoted_status: term, created_at: term, user: term, possibly_sensitive: term, truncated: term, in_reply_to_status_id: term, source: term, extended_entities: term, favorited: term, text: term, withheld_in_countries: term, lang: term, id: term, withheld_copyright: term, retweet_count: term, place: term, current_user_retweet: term, coordinates: term, retweeted_status: term, in_reply_to_user_id: term, full_text: term, in_reply_to_screen_name: term}
 
-  defp send_reply(tweet) do
-    IO.puts "Sending reply"
-    IO.puts "#{tweet.user.screen_name}: #{tweet.text}\n---------------\n"
+  defp send_reply(tweet, hashtag) do
+    IO.puts "Sending reply for hashtag: #{hashtag}"
+    # IO.puts "#{tweet.user.screen_name}: #{tweet.text}\n---------------\n"
     # if tweet.user.screen_name == "chef_tschenko" do
     #   IO.puts "Wont reply to my own tweet."
     # else
-    ExTwitter.update("Ja genau: #{tweet.user.screen_name}, #{tweet.id_str}")
+    # ExTwitter.update("Ja genau: #{tweet.user.screen_name}, #{tweet.id_str}")
     # end
   end
 
