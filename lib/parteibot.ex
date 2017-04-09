ae528f074ecdb9bb4d2dd00480d2ebb5b67a4979
Eduard Litau <eduard.litau@gmail.com>
2017-04-09 20:10:25 +0200

    Fixed configuration

--- lib/parteibot.ex
+++ lib/parteibot.ex
@@ -13,7 +13,7 @@ defmodule Parteibot do
         # Start the endpoint when the application starts
         supervisor(Parteibot.Endpoint, []),
         # Start your own worker by calling: Parteibot.Worker.start_link(arg1, arg2, arg3)
-        worker(Parteibot.StreamWatcherSupervisor, []),
+        supervisor(Parteibot.StreamWatcherSupervisor, []),
       ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
