231e5dc7395ebd5142158d330d583d9e4aa67ccf
Eduard Litau <eduard.litau@gmail.com>
2019-04-10 18:10:09 +0200

    Small fixes

--- lib/web/models/twitter_stream_worker.ex
+++ lib/web/models/twitter_stream_worker.ex
@@ -54,7 +54,7 @@ defmodule Parteibot.TwitterStreamWorker do
 
   # t :: %ExTwitter.Model.Tweet{contributors: term, in_reply_to_status_id_str: term, filter_level: term, favorite_count: term, withheld_scope: term, scopes: term, geo: term, display_text_range: term, retweeted: term, entities: term, id_str: term, quoted_status_id_str: term, quoted_status_id: term, in_reply_to_user_id_str: term, quoted_status: term, created_at: term, user: term, possibly_sensitive: term, truncated: term, in_reply_to_status_id: term, source: term, extended_entities: term, favorited: term, text: term, withheld_in_countries: term, lang: term, id: term, withheld_copyright: term, retweet_count: term, place: term, current_user_retweet: term, coordinates: term, retweeted_status: term, in_reply_to_user_id: term, full_text: term, in_reply_to_screen_name: term}
 
-  defp send_reply(tweet, hashtag) do
+  defp send_reply(_tweet, hashtag) do
     IO.puts("Sending reply for hashtag: #{hashtag}")
     # IO.puts "#{tweet.user.screen_name}: #{tweet.text}\n---------------\n"
     # if tweet.user.screen_name == "chef_tschenko" do
