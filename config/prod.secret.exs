7436cf938c663b2695446255ebe0bd88e3190dee
Eduard Litau <eduard.litau@gmail.com>
2019-04-10 15:47:47 +0200

    Seems unused

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,20 +1,20 @@
-use Mix.Config
+# use Mix.Config
 
-# In this file, we keep production configuration that
-# you likely want to automate and keep it away from
-# your version control system.
-#
-# You should document the content of this
-# file or create a script for recreating it, since it's
-# kept out of version control and might be hard to recover
-# or recreate for your teammates (or you later on).
-config :parteibot, Web.Endpoint,
-  secret_key_base: "RREc78cAFcXQQsL8MMpCohm1Uy3Cvje5+i00fpUxAb5vVNtPMdMOZPXHTOGPOJqh"
+# # In this file, we keep production configuration that
+# # you likely want to automate and keep it away from
+# # your version control system.
+# #
+# # You should document the content of this
+# # file or create a script for recreating it, since it's
+# # kept out of version control and might be hard to recover
+# # or recreate for your teammates (or you later on).
+# config :parteibot, Parteibot.Endpoint,
+#   secret_key_base: "RREc78cAFcXQQsL8MMpCohm1Uy3Cvje5+i00fpUxAb5vVNtPMdMOZPXHTOGPOJqh"
 
-# Configure your database
-config :parteibot, Parteibot.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "parteibot_prod",
-  pool_size: 20
+# # Configure your database
+# config :parteibot, Parteibot.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   username: "postgres",
+#   password: "postgres",
+#   database: "parteibot_prod",
+#   pool_size: 20
